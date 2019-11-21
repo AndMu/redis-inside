a31ddf5dc7897e3769c6bbceb5127268a732f933
Andrius Mudinas <keistokas@gmail.com>
2019-11-21 15:24:46 +0000

    Improvments to test instance

--- source/RedisInside/Redis.cs
+++ source/RedisInside/Redis.cs
@@ -1,11 +1,11 @@
-﻿using Polly;
-using StackExchange.Redis;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Text;
+using Polly;
+using StackExchange.Redis;
 
 namespace RedisInside
 {
@@ -28,7 +28,11 @@ namespace RedisInside
         {
             configuration?.Invoke(config);
 
-            executable = new TemporaryFile(GetType().GetTypeInfo().Assembly.GetManifestResourceStream("RedisInside.Executables.redis-server.exe"), config.Location, "exe");
+            executable = new TemporaryFile(
+                GetType().GetTypeInfo().Assembly.GetManifestResourceStream("RedisInside.Executables.redis-server.exe"),
+                config.Location,
+                "exe");
+
             var processStartInfo = new ProcessStartInfo(" \"" + executable.Info.FullName + " \"")
             {
                 UseShellExecute = false,
@@ -42,30 +46,36 @@ namespace RedisInside
 
             Kill();
             process = Process.Start(processStartInfo);
-            process.ErrorDataReceived += (sender, args) => config.Logger.Invoke(args.Data);
-            process.OutputDataReceived += (sender, args) => config.Logger.Invoke(args.Data);
+            process.ErrorDataReceived += (sender, args) => Log(args.Data);
+            process.OutputDataReceived += (sender, args) => Log(args.Data);
             process.BeginOutputReadLine();
             CheckStatus();
         }
 
         public EndPoint Endpoint => new IPEndPoint(IPAddress.Loopback, config.SelectedPort);
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Log(string message)
+        {
+            config.Logger?.Invoke(message);
+        }
+
         private void CheckStatus()
         {
             var option = new ConfigurationOptions
             {
-                AbortOnConnectFail = false,
+                AbortOnConnectFail = true,
                 EndPoints = { Endpoint },
                 AllowAdmin = true
             };
 
             multiplexer = ConnectionMultiplexer.Connect(option);
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            var status = multiplexer.GetStatus();
         }
 
         protected virtual void Dispose(bool disposing)
@@ -75,24 +85,21 @@ namespace RedisInside
                 return;
             }
 
-            if (disposing)
+            if (!disposing)
             {
-                StopServer();
-                ShutdownProcess();
-                Kill();
-
-                Policy
-                   .Handle<Exception>()
-                   .WaitAndRetry(new[]
-                                 {
-                                     TimeSpan.FromSeconds(1),
-                                     TimeSpan.FromSeconds(2),
-                                     TimeSpan.FromSeconds(3)
-                                 },
-                                 (exception, timeSpan) => { config.Logger("Failed to delete files..."); })
-                   .Execute(DeleteFiles);
+                return;
             }
 
+            Log("Dispose");
+            StopServer();
+            ShutdownProcess();
+            Kill();
+
+            Policy
+                .Handle<Exception>()
+                .WaitAndRetry(new[] { TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(3) },
+                              (exception, timeSpan) => { Log("Failed to delete files..."); })
+                .Execute(DeleteFiles);
             disposed = true;
         }
 
@@ -103,6 +110,7 @@ namespace RedisInside
                 return;
             }
 
+            Log("Kill");
             foreach (var redis in Process.GetProcessesByName(Path.GetFileNameWithoutExtension(executable.Info.Name)))
             {
                 try
@@ -111,7 +119,7 @@ namespace RedisInside
                 }
                 catch (Exception e)
                 {
-                    config.Logger(e.Message);
+                    Log(e.Message);
                 }
             }
         }
@@ -123,22 +131,23 @@ namespace RedisInside
                 if (config.IsWithPersistence)
                 {
                     var file = Path.Combine(executable.Info.DirectoryName, config.PersistenceFile);
+
                     if (File.Exists(file))
                     {
-                        config.Logger($"Deleting DB: {file}");
+                        Log($"Deleting DB: {file}");
                         File.Delete(file);
                     }
                 }
 
                 if (File.Exists(executable.Info.FullName))
                 {
-                    config.Logger($"Deleting: {executable.Info.FullName}");
+                    Log($"Deleting: {executable.Info.FullName}");
                     File.Delete(executable.Info.FullName);
                 }
             }
             catch (Exception ex)
             {
-                config.Logger(ex.ToString());
+                Log(ex.ToString());
             }
         }
 
@@ -147,18 +156,19 @@ namespace RedisInside
             try
             {
                 process.CancelOutputRead();
-                config.Logger("Redis was started for this test run. Shutting down");
+                Log("Redis was started for this test run. Shutting down");
+
                 if (process != null)
                 {
                     if (process.HasExited)
                     {
-                        config.Logger("Process already existed");
+                        Log("Process already existed");
                     }
                     else
                     {
                         if (!process.CloseMainWindow())
                         {
-                            config.Logger("Close failed");
+                            Log("Close failed");
                             process.Kill();
                         }
                         else
@@ -173,21 +183,23 @@ namespace RedisInside
             }
             catch (Exception ex)
             {
-                config.Logger(ex.ToString());
+                Log(ex.ToString());
             }
         }
 
         private void StopServer()
         {
+            Log("StopServer");
             try
             {
                 multiplexer.GetServer(Endpoint).Shutdown();
-                multiplexer.Dispose();
             }
             catch (Exception ex)
             {
-                config.Logger(ex.ToString());
+                Log(ex.ToString());
             }
+
+            multiplexer.Dispose();
         }
     }
 }
